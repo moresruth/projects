f6b92de274866b59e46e7b78363cb94c48e4e208
Ruth Mores <moresruthantonette@gmail.com>
2022-09-10 12:36:00 +0800

    add assets

--- landingpage_responsive/src/components/Navbar/Navbar.jsx
+++ landingpage_responsive/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import {RiMenuLine, RiCloseline} from 'react-icons/ri';
+
 import './navbar.css';
+
+// BEM --> Block ELement Modifier
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className = "gpt3__navbar">
+      <div className="gpt3__navbar-links">
+          <div className="gpt3__navbar-links_logo">
+            <img src={logo} alt="logo" />
+          </div>
+      </div>
+    </div>
   )
 }
 
