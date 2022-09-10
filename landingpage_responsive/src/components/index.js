44d3400b2e8238ce4c513fbc107b78591c5f186d
Ruth Mores <moresruthantonette@gmail.com>
2022-09-10 10:54:15 +0800

    modified

--- landingpage_responsive/src/components/index.js
+++ landingpage_responsive/src/components/index.js
@@ -2,5 +2,5 @@
 export{default as Article} from './Article/Article';
 export{default as Brand} from './Brand/Brand';
 export{default as CTA} from './CTA/CTA';
-export{default as Feature} from './Feature';
+export{default as Feature} from './Feature/Feature';
 export{default as Navbar} from './Navbar/Navbar';
--- landingpage_responsive/src/containers/index.js
+++ landingpage_responsive/src/containers/index.js
@@ -3,5 +3,5 @@ export {default as Blog} from './Blog/Blog';
 export {default as Features} from './Features/Features';
 export {default as Footer} from './Footer/Footer';
 export {default as Header} from './Header/Header';
-export {default as Possibility} from './Possibility/';
+export {default as Possibility} from './Possibility/Possibility';
 export {default as WhatGPT3} from './whatGPT3/WhatGPT3';
