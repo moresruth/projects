e6817ee3e7892b6b435d4da01a27e5d279d700fb
Ruth Mores <moresruthantonette@gmail.com>
2022-09-11 23:40:26 +0800

    modified

--- landingpage_responsive/src/containers/Header/Header.jsx
+++ landingpage_responsive/src/containers/Header/Header.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import './header.css';
 import people from '../../assets/people.png';
-import ai from '../../assets/ai.png';
+import profile from '../../assets/profile.png';
 
 
 const Header = () => (
 
     <div className="gpt3__header section__padding" id="home">
       <div className="gpt3__header-content">
-        <h1 className="gradient__text">Let's Build Something amazing with GPT-3 Open AI</h1>
-        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment.
-           Party we years to order allow asked of.</p>
+        <p>👋 Hi, I'm</p>
+        <h1 className="gradient__text"> Antonette Mores</h1>
+        <p>  Hey! How nice of you to look at my personal site, Thank you! <br/>
+      I am Front-End Developer that specializes at responsive web design.</p>
 
         <div className="gpt3__header-content__input">
           <input type="email" placeholder='Your email address'/>
@@ -24,7 +25,7 @@ const Header = () => (
       </div>
 
       <div className="gpt3__header-image">
-          <img src={ai} alt="ai" />
+          <img src={profile} alt="ai" />
       </div>
     </div>
   );
